3f7565fc9804dc854ff1c35b50e0bc6613a69b1a
Steven Indzeoski <steven.indzeoski@me.com>
2017-02-02 23:36:43 -0600

    #10: Creating SwissPlayerFormComponent. Moving HTML into new component.

---
+++ src/app/swiss/swiss-player-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'mm-swiss-player-form',
+  templateUrl: 'swiss-player-form.html'
+})
+export class SwissPlayerFormComponent {
+  swissPlayerForm: FormGroup;
+}
