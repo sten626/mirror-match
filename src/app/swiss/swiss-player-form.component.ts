7ab9acc8262e71d24cf8fb6eb35444c8c943ae6e
Steven Indzeoski <steven.indzeoski@me.com>
2017-02-04 15:22:58 -0600

    #10: Continuing to experiment with reactive forms and splitting up the players page.

--- src/app/swiss/swiss-player-form.component.ts
+++ src/app/swiss/swiss-player-form.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Player, PlayerService } from '../shared';
 
 @Component({
   selector: 'mm-swiss-player-form',
-  templateUrl: 'swiss-player-form.html'
+  templateUrl: 'swiss-player-form.component.html'
 })
-export class SwissPlayerFormComponent {
+export class SwissPlayerFormComponent implements OnChanges {
+  @Input() player: Player;
+
+  errors = {};
   swissPlayerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private playerService: PlayerService
+  ) {
+    this.createForm();
+  }
+
+  ngOnChanges() {
+    this.swissPlayerForm.reset({
+      name: this.player.name
+    });
+  }
+
+  onSubmit() {
+    this.updatePlayer();
+    this.playerService.save(this.player).subscribe(() => {
+      this.swissPlayerForm.reset();
+      // TODO: Emit event to unselect a player.
+    }, err => {
+      this.errors = err;
+    });
+  }
+
+  private createForm() {
+    this.swissPlayerForm = this.fb.group({
+      name: ['', Validators.required]
+    });
+  }
+
+  private updatePlayer() {
+    Object.assign(this.player, this.swissPlayerForm.value);
+  }
 }
