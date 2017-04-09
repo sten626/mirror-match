71ef275fcb14704abd9a5ced87b3058728b905fa
Steven Indzeoski <steven@Stevens-Mac-Pro.local>
2017-04-08 21:42:22 -0600

    #18: Reworking PlayerService: - Downgrading tslint. 5.0.0 is incompatible with codelyzer. - Refactoring PlayerService to be more reactive.

--- src/app/shared/services/player.service.ts
+++ src/app/shared/services/player.service.ts
@@ -1,88 +1,62 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/count';
 import 'rxjs/add/operator/distinctUntilChanged';
 
 import { Player } from '../models';
 
 @Injectable()
 export class PlayerService {
-  numPlayers = 0;
-  private nextId = 1;
-  private players: Player[];
-  private playersCache = {};
-  private playersKey = 'players';
+  numberOfPlayers: Observable<number>;
+  players: Observable<Player[]>;
+  recommendedNumberOfRounds: Observable<number>;
+
+  private nextId: number;
+  private _players: Player[];
   private playersSubject = new BehaviorSubject<Player[]>([]);
 
-  delete(player: Player): Observable<boolean> {
-    delete this.playersCache[player.id];
-    this.players.splice(this.players.indexOf(player), 1);
-    this.saveToLocalStorage();
-    this.playersSubject.next(this.players.slice());
-    this.numPlayers = this.players.length;
+  private readonly lsKeys = {
+    players: 'players'
+  };
 
-    const deleteObservable = new Observable(observer => {
-      observer.next(true);
-      observer.complete();
-    });
+  constructor() {
+    this.loadFromLocalStorage();
 
-    return deleteObservable;
+    // Setup Observables.
+    this.players = this.playersSubject.asObservable().distinctUntilChanged();
+    this.numberOfPlayers = this.players.count().distinctUntilChanged();
+    this.recommendedNumberOfRounds = this.numberOfPlayers.map(num => {
+      return Math.max(3, Math.ceil(Math.log2(num)));
+    }).distinctUntilChanged();
+
+    this.playersSubject.next(this._players.slice());
   }
 
-  get(id: number): Player {
-    if (!this.players) {
-      this.loadFromLocalStorage();
+  delete(player: Player): void {
+    if (player) {
+      const playerIndex = this._players.indexOf(player);
+      this._players.splice(playerIndex, 1);
+      this.saveToLocalStorage();
+      this.playersSubject.next(this._players.slice());
     }
-
-    if (!this.playersCache[id]) {
-      const player = this.players.filter(p => p.id === id);
-
-      if (player.length === 1) {
-        this.playersCache[id] = player[0];
-      }
-    }
-
-    return this.playersCache[id];
   }
 
-  getAll(): Observable<Player[]> {
-    if (!this.players) {
-      this.loadFromLocalStorage();
-    }
-
-    this.numPlayers = this.players.length;
-    this.playersSubject.next(this.players.slice());
-
-    return this.playersSubject.asObservable().distinctUntilChanged();
-  }
-
-  getRecommendedNumberOfRounds(): number {
-    return Math.max(3, Math.ceil(Math.log2(this.players.length)));
-  }
-
-  save(player: Player): Observable<Player> {
+  save(player: Player): void {
     if (!player.id) {
       // New player.
       player.id = this.nextId++;
-      this.players.push(player);
-      this.playersCache[player.id] = player;
+      this._players.push(player);
     }
 
+    // TODO: Save existing players?
     this.saveToLocalStorage();
-    this.numPlayers = this.players.length;
-    this.playersSubject.next(this.players.slice());
-
-    const playerObservable = new Observable(observer => {
-      observer.next(player);
-      observer.complete();
-    });
-
-    return playerObservable;
+    this.playersSubject.next(this._players.slice());
   }
 
   private initNextId() {
-    if (this.players.length > 0) {
-      const ids = this.players.map(player => player.id);
+    if (this._players.length > 0) {
+      const ids = this._players.map(player => player.id);
       this.nextId = Math.max(...ids) + 1;
     } else {
       this.nextId = 1;
@@ -90,30 +64,45 @@ export class PlayerService {
   }
 
   private loadFromLocalStorage() {
-    const playersData = localStorage.getItem(this.playersKey);
+    const playersData = localStorage.getItem(this.lsKeys.players);
 
     if (playersData) {
-      // this.players = JSON.parse(playersData);
       const playersRawArray = JSON.parse(playersData);
-      this.players = playersRawArray.map(rawPlayer => {
+      this._players = playersRawArray.map(rawPlayer => {
         return new Player(rawPlayer.id, rawPlayer.name);
       });
     } else {
-      this.players = [];
-      localStorage.setItem(this.playersKey, JSON.stringify(this.players));
+      this._players = [];
+      localStorage.setItem(this.lsKeys.players, JSON.stringify(this._players));
     }
 
     this.initNextId();
   }
 
   private saveToLocalStorage() {
-    const playersToLocalStorage = this.players.map(player => {
+    const playersToLocalStorage = this._players.map(player => {
       return {
         id: player.id,
         name: player.name
       };
     });
 
-    localStorage.setItem(this.playersKey, JSON.stringify(playersToLocalStorage));
+    localStorage.setItem(this.lsKeys.players, JSON.stringify(playersToLocalStorage));
   }
+
+  // get(id: number): Player {
+  //   if (!this.players) {
+  //     this.loadFromLocalStorage();
+  //   }
+
+  //   if (!this.playersCache[id]) {
+  //     const player = this.players.filter(p => p.id === id);
+
+  //     if (player.length === 1) {
+  //       this.playersCache[id] = player[0];
+  //     }
+  //   }
+
+  //   return this.playersCache[id];
+  // }
 }
