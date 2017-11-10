b5b6cc4ab4afac71c5e395bd95ed685aaefd3858
Steven Indzeoski <steven.indzeoski@gmail.com>
2017-11-09 22:23:18 -0600

    #27: Fixing weird indentation in RoundService. - Not sure how that happened.

--- src/app/shared/services/round.service.ts
+++ src/app/shared/services/round.service.ts
@@ -9,164 +9,164 @@ import { PlayerService } from './player.service';
 
 @Injectable()
 export class RoundService {
-    canBeginTournament: Observable<boolean>;
-    canStartNextRound: Observable<boolean>;
-    completedRounds: Observable<number[]>;
-    hasBegunTournament: Observable<boolean>;
-    hasCompletedRounds: Observable<boolean>;
-    pairingsForSelectedRound: Observable<Pairing[]>;
-    rounds: Observable<number[]>;
-    selectedRound: Observable<number>;
-    selectedRoundComplete: Observable<boolean>;
-    selectedRoundHasPairings: Observable<boolean>;
-    selectedRoundHasSubmittedPairings: Observable<boolean>;
+  canBeginTournament: Observable<boolean>;
+  canStartNextRound: Observable<boolean>;
+  completedRounds: Observable<number[]>;
+  hasBegunTournament: Observable<boolean>;
+  hasCompletedRounds: Observable<boolean>;
+  pairingsForSelectedRound: Observable<Pairing[]>;
+  rounds: Observable<number[]>;
+  selectedRound: Observable<number>;
+  selectedRoundComplete: Observable<boolean>;
+  selectedRoundHasPairings: Observable<boolean>;
+  selectedRoundHasSubmittedPairings: Observable<boolean>;
 
-    private _completedRounds: number[];
-    private completedRoundsSubject = new BehaviorSubject<number[]>([]);
-    private players: Player[];
-    private _rounds: number[];
-    private roundsSubject = new BehaviorSubject<number[]>([]);
-    private _selectedRound: number;
-    private selectedRoundSubject: BehaviorSubject<number>;
-    private totalNumberOfRounds: number;
+  private _completedRounds: number[];
+  private completedRoundsSubject = new BehaviorSubject<number[]>([]);
+  private players: Player[];
+  private _rounds: number[];
+  private roundsSubject = new BehaviorSubject<number[]>([]);
+  private _selectedRound: number;
+  private selectedRoundSubject: BehaviorSubject<number>;
+  private totalNumberOfRounds: number;
 
-    private readonly lsKeys = {
-      completedRounds: 'completedRounds',
-      rounds: 'rounds',
-      totalNumberOfRounds: 'totalNumberOfRounds'
-    };
+  private readonly lsKeys = {
+    completedRounds: 'completedRounds',
+    rounds: 'rounds',
+    totalNumberOfRounds: 'totalNumberOfRounds'
+  };
 
-    constructor(
-      private pairingService: PairingService,
-      private playerService: PlayerService
-    ) {
-      this.loadFromLocalStorage();
+  constructor(
+    private pairingService: PairingService,
+    private playerService: PlayerService
+  ) {
+    this.loadFromLocalStorage();
 
-      // Setup Observables.
-      this.canBeginTournament = this.playerService.numberOfPlayers.pipe(
-        map((numPlayers: number) => numPlayers >= 4),
-        distinctUntilChanged()
-      );
-      this.rounds = this.roundsSubject.asObservable().pipe(distinctUntilChanged());
-      this.completedRounds = this.completedRoundsSubject.asObservable().pipe(distinctUntilChanged());
-      this.hasCompletedRounds = this.completedRounds.pipe(map((rounds: number[]) => rounds.length > 0), distinctUntilChanged());
-      this.hasBegunTournament = this.rounds.pipe(map((rounds: number[]) => rounds.length > 0), distinctUntilChanged());
-      this._selectedRound = Math.max(...this._rounds);
-      this.selectedRoundSubject = new BehaviorSubject<number>(this._selectedRound);
-      this.selectedRound = this.selectedRoundSubject.asObservable().pipe(distinctUntilChanged());
-      this.pairingsForSelectedRound = this.pairingService.pairings.pipe(
-        combineLatest(this.selectedRound, (pairings: Pairing[], round: number) => {
-          return pairings.filter((pairing: Pairing) => pairing.round === round);
-        })
-      );
+    // Setup Observables.
+    this.canBeginTournament = this.playerService.numberOfPlayers.pipe(
+      map((numPlayers: number) => numPlayers >= 4),
+      distinctUntilChanged()
+    );
+    this.rounds = this.roundsSubject.asObservable().pipe(distinctUntilChanged());
+    this.completedRounds = this.completedRoundsSubject.asObservable().pipe(distinctUntilChanged());
+    this.hasCompletedRounds = this.completedRounds.pipe(map((rounds: number[]) => rounds.length > 0), distinctUntilChanged());
+    this.hasBegunTournament = this.rounds.pipe(map((rounds: number[]) => rounds.length > 0), distinctUntilChanged());
+    this._selectedRound = Math.max(...this._rounds);
+    this.selectedRoundSubject = new BehaviorSubject<number>(this._selectedRound);
+    this.selectedRound = this.selectedRoundSubject.asObservable().pipe(distinctUntilChanged());
+    this.pairingsForSelectedRound = this.pairingService.pairings.pipe(
+      combineLatest(this.selectedRound, (pairings: Pairing[], round: number) => {
+        return pairings.filter((pairing: Pairing) => pairing.round === round);
+      })
+    );
 
-      this.selectedRoundHasPairings = this.pairingsForSelectedRound.pipe(
-        map((pairings: Pairing[]) => {
-          return pairings.length > 0;
-        }),
-        distinctUntilChanged()
-      );
+    this.selectedRoundHasPairings = this.pairingsForSelectedRound.pipe(
+      map((pairings: Pairing[]) => {
+        return pairings.length > 0;
+      }),
+      distinctUntilChanged()
+    );
 
-      this.selectedRoundComplete = this.pairingsForSelectedRound.pipe(
-        map((pairings: Pairing[]) => {
-          if (pairings.length === 0) {
-            return false;
-          }
+    this.selectedRoundComplete = this.pairingsForSelectedRound.pipe(
+      map((pairings: Pairing[]) => {
+        if (pairings.length === 0) {
+          return false;
+        }
 
-          return pairings
-            .map((pairing: Pairing) => pairing.submitted)
-            .reduce((allSubmitted: boolean, submitted: boolean) => allSubmitted && submitted);
-        }),
-        distinctUntilChanged()
-      );
+        return pairings
+          .map((pairing: Pairing) => pairing.submitted)
+          .reduce((allSubmitted: boolean, submitted: boolean) => allSubmitted && submitted);
+      }),
+      distinctUntilChanged()
+    );
 
-      this.selectedRoundHasSubmittedPairings = this.pairingsForSelectedRound.map((pairings: Pairing[]) => {
-        const submitted = pairings.filter((pairing: Pairing) => pairing.submitted);
+    this.selectedRoundHasSubmittedPairings = this.pairingsForSelectedRound.map((pairings: Pairing[]) => {
+      const submitted = pairings.filter((pairing: Pairing) => pairing.submitted);
 
-        return submitted.length > 0;
-      });
+      return submitted.length > 0;
+    });
 
-      this.canStartNextRound = this.rounds.pipe(combineLatest(this.completedRounds, (rounds: number[], completedRounds: number[]) => {
-        return rounds.length === completedRounds.length;
-      }));
+    this.canStartNextRound = this.rounds.pipe(combineLatest(this.completedRounds, (rounds: number[], completedRounds: number[]) => {
+      return rounds.length === completedRounds.length;
+    }));
 
-      this.playerService.players.subscribe((players: Player[]) => this.players = players);
+    this.playerService.players.subscribe((players: Player[]) => this.players = players);
 
-      this.roundsSubject.next(this._rounds.slice());
-      this.completedRoundsSubject.next(this._completedRounds.slice());
+    this.roundsSubject.next(this._rounds.slice());
+    this.completedRoundsSubject.next(this._completedRounds.slice());
+  }
+
+  createNextRound(): void {
+    const nextRound = this._rounds.length > 0 ? Math.max(...this._rounds) + 1 : 1;
+    this._rounds.push(nextRound);
+    this.saveToLocalStorage();
+    this.roundsSubject.next(this._rounds.slice());
+    this._selectedRound = nextRound;
+    this.selectedRoundSubject.next(this._selectedRound);
+  }
+
+  markRoundAsComplete(round: number): void {
+    if (!round || round < 1) {
+      return;
     }
 
-    createNextRound(): void {
-      const nextRound = this._rounds.length > 0 ? Math.max(...this._rounds) + 1 : 1;
-      this._rounds.push(nextRound);
-      this.saveToLocalStorage();
-      this.roundsSubject.next(this._rounds.slice());
-      this._selectedRound = nextRound;
-      this.selectedRoundSubject.next(this._selectedRound);
+    if (this._completedRounds.indexOf(round) !== -1) {
+      return;
     }
 
-    markRoundAsComplete(round: number): void {
-      if (!round || round < 1) {
-        return;
-      }
+    this._completedRounds.push(round);
+    this.saveToLocalStorage();
+    this.completedRoundsSubject.next(this._completedRounds.slice());
+  }
 
-      if (this._completedRounds.indexOf(round) !== -1) {
-        return;
-      }
-
-      this._completedRounds.push(round);
-      this.saveToLocalStorage();
-      this.completedRoundsSubject.next(this._completedRounds.slice());
+  markRoundAsIncomplete(round: number): void {
+    if (!round || round < 1) {
+      return;
     }
 
-    markRoundAsIncomplete(round: number): void {
-      if (!round || round < 1) {
-        return;
-      }
+    const roundIndex = this._completedRounds.indexOf(round);
 
-      const roundIndex = this._completedRounds.indexOf(round);
-
-      if (roundIndex === -1) {
-        return;
-      }
-
-      this._completedRounds.splice(roundIndex, 1);
-      this.saveToLocalStorage();
-      this.completedRoundsSubject.next(this._completedRounds.slice());
+    if (roundIndex === -1) {
+      return;
     }
 
-    setSelectedRound(selectedRound: number): void {
-      this._selectedRound = selectedRound;
-      this.selectedRoundSubject.next(this._selectedRound);
-    }
+    this._completedRounds.splice(roundIndex, 1);
+    this.saveToLocalStorage();
+    this.completedRoundsSubject.next(this._completedRounds.slice());
+  }
 
-    setTotalNumberOfRounds(numberOfRounds: number): void {
-      this.totalNumberOfRounds = numberOfRounds;
-      localStorage.setItem(this.lsKeys.totalNumberOfRounds, JSON.stringify(this.totalNumberOfRounds));
-    }
+  setSelectedRound(selectedRound: number): void {
+    this._selectedRound = selectedRound;
+    this.selectedRoundSubject.next(this._selectedRound);
+  }
 
-    private loadFromLocalStorage(): void {
-      const roundsData = localStorage.getItem(this.lsKeys.rounds);
+  setTotalNumberOfRounds(numberOfRounds: number): void {
+    this.totalNumberOfRounds = numberOfRounds;
+    localStorage.setItem(this.lsKeys.totalNumberOfRounds, JSON.stringify(this.totalNumberOfRounds));
+  }
 
-      if (roundsData) {
-        this._rounds = JSON.parse(roundsData);
-      } else {
-        this._rounds = [];
-        localStorage.setItem(this.lsKeys.rounds, JSON.stringify(this._rounds));
-      }
+  private loadFromLocalStorage(): void {
+    const roundsData = localStorage.getItem(this.lsKeys.rounds);
 
-      const completedRoundsData = localStorage.getItem(this.lsKeys.completedRounds);
-
-      if (completedRoundsData) {
-        this._completedRounds = JSON.parse(completedRoundsData);
-      } else {
-        this._completedRounds = [];
-        localStorage.setItem(this.lsKeys.completedRounds, JSON.stringify(this._completedRounds));
-      }
-    }
-
-    private saveToLocalStorage(): void {
+    if (roundsData) {
+      this._rounds = JSON.parse(roundsData);
+    } else {
+      this._rounds = [];
       localStorage.setItem(this.lsKeys.rounds, JSON.stringify(this._rounds));
+    }
+
+    const completedRoundsData = localStorage.getItem(this.lsKeys.completedRounds);
+
+    if (completedRoundsData) {
+      this._completedRounds = JSON.parse(completedRoundsData);
+    } else {
+      this._completedRounds = [];
       localStorage.setItem(this.lsKeys.completedRounds, JSON.stringify(this._completedRounds));
     }
+  }
+
+  private saveToLocalStorage(): void {
+    localStorage.setItem(this.lsKeys.rounds, JSON.stringify(this._rounds));
+    localStorage.setItem(this.lsKeys.completedRounds, JSON.stringify(this._completedRounds));
+  }
 }
