297ea3ee7dc37c8ba8eeb81bf6277e17662cae06
Steven Indzeoski <steven.indzeoski@gmail.com>
2018-04-11 20:23:12 -0600

    #40: Started reproducing/testing weird bye scenario.

--- src/app/shared/services/pairing.service.spec.ts
+++ src/app/shared/services/pairing.service.spec.ts
@@ -67,4 +67,79 @@ describe('PairingService', () => {
       done();
     });
   });
+
+  it('#createPairings test bad case change this name', () => {
+    const playerA = new Player({
+      id: 1,
+      name: 'A',
+      opponentIds: [2, 3],
+      matchesPlayed: 2,
+      matchesWon: 2
+    });
+    const playerB = new Player({
+      id: 2,
+      name: 'B',
+      opponentIds: [1, 9],
+      matchesPlayed: 2,
+      matchesWon: 1
+    });
+    const playerC = new Player({
+      id: 3,
+      name: 'C',
+      opponentIds: [1, 4],
+      matchesPlayed: 2,
+      matchesWon: 1
+    });
+    const playerD = new Player({
+      id: 4,
+      name: 'D',
+      opponentIds: [3, 6],
+      matchesPlayed: 2,
+      matchesWon: 1
+    });
+    const playerE = new Player({
+      id: 5,
+      name: 'E',
+      opponentIds: [6, 7],
+      matchesPlayed: 2,
+      matchesWon: 2
+    });
+    // const playerF = new Player({
+    //   id: 6,
+    //   name: 'F',
+    //   opponentIds: [4, 5],
+    //   matchesPlayed: 2
+    // });
+    const playerG = new Player({
+      id: 7,
+      name: 'G',
+      opponentIds: [5, 8],
+      matchesPlayed: 2,
+      matchesWon: 1
+    });
+    const playerH = new Player({
+      id: 8,
+      name: 'H',
+      opponentIds: [7],
+      matchesPlayed: 1,
+      byes: 1
+    });
+    const playerI = new Player({
+      id: 9,
+      name: 'I',
+      opponentIds: [2],
+      matchesPlayed: 1,
+      byes: 1
+    });
+    const players = [playerA, playerB, playerC, playerD, playerE, playerG, playerH, playerI];
+    const playersSubject = new BehaviorSubject<Player[]>(players);
+    const fakePlayerService = {
+      activePlayers: playersSubject.asObservable()
+    };
+    pairingService = new PairingService(fakePlayerService as PlayerService);
+    pairingService.createPairings(3, false);
+    pairingService.pairings.subscribe((pairings: Pairing[]) => {
+      expect(pairings.length).toBe(4);
+    });
+  });
 });
