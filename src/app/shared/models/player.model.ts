6a894416954b7e77b0318ffd7078cc23e82ceac5
Steven Indzeoski <steven.indzeoski@gmail.com>
2018-06-23 11:01:43 -0600

    #62: Changing player model to an interface.

--- src/app/shared/models/player.model.ts
+++ src/app/shared/models/player.model.ts
@@ -1,4 +1,4 @@
-export class Player {
+export interface Player {
   id: number;
   name: string;
   matchesPlayed?: number;
@@ -15,23 +15,4 @@ export class Player {
   opponentGameWinPercentage?: number;
   opponentIds?: number[];
   dropped?: boolean;
-
-  // constructor(rawPlayer?: any) {
-  //   this.id = rawPlayer && rawPlayer.id || null;
-  //   this.name = rawPlayer && rawPlayer.name || '';
-  //   this.matchesPlayed = rawPlayer && rawPlayer.matchesPlayed || 0;
-  //   this.matchesWon = rawPlayer && rawPlayer.matchesWon || 0;
-  //   this.matchesDrawn = rawPlayer && rawPlayer.matchesDrawn || 0;
-  //   this.gamesPlayed = rawPlayer && rawPlayer.gamesPlayed || 0;
-  //   this.gamesWon = rawPlayer && rawPlayer.gamesWon || 0;
-  //   this.gamesDrawn = rawPlayer && rawPlayer.gamesDrawn || 0;
-  //   this.byes = rawPlayer && rawPlayer.byes || 0;
-  //   this.matchPoints = rawPlayer && rawPlayer.matchPoints || 0;
-  //   this.gamePoints = rawPlayer && rawPlayer.gamePoints || 0;
-  //   this.opponentMatchWinPercentage = rawPlayer && rawPlayer.opponentMatchWinPercentage || 0;
-  //   this.gameWinPercentage = rawPlayer && rawPlayer.gameWinPercentage || 0;
-  //   this.opponentGameWinPercentage = rawPlayer && rawPlayer.opponentGameWinPercentage || 0;
-  //   this.opponentIds = rawPlayer && rawPlayer.opponentIds || [];
-  //   this.dropped = rawPlayer && rawPlayer.dropped || false;
-  // }
 }
