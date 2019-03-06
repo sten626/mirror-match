8dbaadc3e678d76de0bb187f939d18358010d8d8
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-03-06 08:55:36 -0600

    #68: Adding more PlayerFormComponent tests.

--- src/app/swiss/components/player-form.component.spec.ts
+++ src/app/swiss/components/player-form.component.spec.ts
@@ -43,19 +43,58 @@ describe('Player Form Component', () => {
 
   describe('submit()', () => {
     it('should do nothing when called in addMode with empty name', () => {
-      component.addPlayer.subscribe(() => fail('addPlayer shouldn\'t have been emitted.'));
-      component.updatePlayerName.subscribe(() => fail('updatePlayerName shouldn\'t have been emitted.'));
+      spyOn(component.addPlayer, 'emit');
+      spyOn(component.updatePlayerName, 'emit');
       component.submit();
+      expect(component.addPlayer.emit).toHaveBeenCalledTimes(0);
+      expect(component.updatePlayerName.emit).toHaveBeenCalledTimes(0);
     });
 
     it('should emit nothing and trim field when called in addMode with whitespace', () => {
       nameEl.value = '   ';
-      component.addPlayer.subscribe(() => fail('addPlayer shouldn\'t have been emitted.'));
-      component.updatePlayerName.subscribe(() => fail('updatePlayerName shouldn\'t have been emitted.'));
+      spyOn(component.addPlayer, 'emit');
+      spyOn(component.updatePlayerName, 'emit');
       component.submit();
       fixture.detectChanges();
 
       expect(nameEl.textContent).toBe('');
+      expect(component.addPlayer.emit).toHaveBeenCalledTimes(0);
+      expect(component.updatePlayerName.emit).toHaveBeenCalledTimes(0);
+    });
+
+    it('should emit addPlayer and clear form when in addMode', () => {
+      const playerName = 'Sten';
+      nameEl.value = playerName;
+      nameEl.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      let addedPlayer: Player;
+      component.addMode = true;
+      component.addPlayer.subscribe((p: Player) => addedPlayer = p);
+      component.submit();
+      fixture.detectChanges();
+      expect(addedPlayer.name).toBe(playerName);
+      expect(nameEl.textContent).toBe('');
+      expect(component.addMode).toBe(true);
+      expect(component.isPlayerDroppable).toBe(false);
+    });
+
+    it('should emit updatePlayerName and playerFormReset when in edit mode', () => {
+      const newPlayerName = 'Steven';
+      let eventPlayer: Player;
+      let eventName: string;
+
+      component.selectedPlayer = player;
+      component.addMode = false;
+      nameEl.value = newPlayerName;
+      nameEl.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      component.updatePlayerName.subscribe((event: any) => {
+        eventPlayer = event.player;
+        eventName = event.name;
+      });
+      component.submit();
+      expect(eventPlayer).toBe(player);
+      expect(eventName).toBe(newPlayerName);
     });
   });
 });
