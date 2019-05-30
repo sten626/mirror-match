0418bf0835b34b51377e69740fd373367e0457fb
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-05-30 08:38:11 -0600

    #68: Starting on fixing up the pairings page.

--- src/app/swiss/containers/pairings-page.component.ts
+++ src/app/swiss/containers/pairings-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { Pairing, PairingService, Player, PlayerService, RoundService, StandingsService } from 'app/shared';
+import { Pairing, Player } from 'app/shared';
 import * as fromSwiss from 'app/swiss/reducers';
 import { Observable, Subscription } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
@@ -20,72 +20,85 @@ export class PairingsPageComponent implements OnDestroy, OnInit {
   selectedRound = 1;
   selectedRoundHasPairings$: Observable<boolean>;
 
-  private roundsSub: Subscription;
+  private currentRoundSub: Subscription;
 
   constructor(
     private fb: FormBuilder,
-    private pairingService: PairingService,
-    private playerService: PlayerService,
-    private roundService: RoundService,
+    // private pairingService: PairingService,
+    // private playerService: PlayerService,
+    // private roundService: RoundService,
     private router: Router,
-    private standingsService: StandingsService,
+    // private standingsService: StandingsService,
     private store: Store<fromSwiss.State>
   ) {
-    this.store = this.store;
-    this.canStartNextRound$ = this.roundService.canStartNextRound$;
-    this.hasTournamentStarted$ = store.pipe(
+    // this.canStartNextRound$ = this.roundService.canStartNextRound$;
+    this.hasTournamentStarted$ = this.store.pipe(
       select(fromSwiss.hasTournamentStarted)
     );
     this.createForm();
   }
 
   ngOnInit() {
-    this.roundsSub = this.roundService.rounds$.subscribe((rounds: number[]) => {
+    this.currentRoundSub = this.store.pipe(
+      select(fromSwiss.getCurrentRound)
+    ).subscribe((currentRound) => {
+      const rounds = [];
+      let i = 1;
+
+      while (i <= currentRound) {
+        rounds.push(i++);
+      }
+
       this.rounds = rounds;
-      const selectedRound = Math.max(...rounds);
-      this.pairingsForm.reset({
-        currentRound: selectedRound
-      });
-      this.selectedRoundChanged(selectedRound);
     });
   }
 
+  // ngOnInit() {
+  //   this.roundsSub = this.roundService.rounds$.subscribe((rounds: number[]) => {
+  //     this.rounds = rounds;
+  //     const selectedRound = Math.max(...rounds);
+  //     this.pairingsForm.reset({
+  //       currentRound: selectedRound
+  //     });
+  //     this.selectedRoundChanged(selectedRound);
+  //   });
+  // }
   ngOnDestroy() {
-    this.roundsSub.unsubscribe();
+    this.currentRoundSub.unsubscribe();
   }
 
   createNextRound(): void {
-    this.roundService.createNextRound();
+    // this.roundService.createNextRound();
   }
 
   generatePairings(): void {
-    const lastRound = this.roundService.getTotalNumberOfRounds();
-    this.pairingService.createPairings(this.selectedRound, this.selectedRound === lastRound);
+    // const lastRound = this.roundService.getTotalNumberOfRounds();
+    // this.pairingService.createPairings(this.selectedRound, this.selectedRound === lastRound);
   }
 
   onLastResultSubmitted(): void {
-    this.roundService.markRoundAsComplete(this.selectedRound);
-    this.standingsService.calculateStandings();
-    this.router.navigate(['/swiss/standings']);
+    // this.roundService.markRoundAsComplete(this.selectedRound);
+    // this.standingsService.calculateStandings();
+    // this.router.navigate(['/swiss/standings']);
   }
 
-  onMatchResultsCleared(pairings: Pairing[]): void {
-    this.pairingService.updatePairings(pairings);
-    this.roundService.markRoundAsIncomplete(this.selectedRound);
-  }
+  // onMatchResultsCleared(pairings: Pairing[]): void {
+  //   this.pairingService.updatePairings(pairings);
+  //   this.roundService.markRoundAsIncomplete(this.selectedRound);
+  // }
 
-  onPlayerDroppedChanged(player: Player): void {
-    this.playerService.updatePlayer(player);
-  }
+  // onPlayerDroppedChanged(player: Player): void {
+  //   this.playerService.updatePlayer(player);
+  // }
 
   onRedoMatchesForRound(): void {
-    this.pairingService.deletePairings(this.selectedRound);
-    this.roundService.markRoundAsIncomplete(this.selectedRound);
+    // this.pairingService.deletePairings(this.selectedRound);
+    // this.roundService.markRoundAsIncomplete(this.selectedRound);
   }
 
-  onResultSubmitted(pairing: Pairing): void {
-    this.pairingService.updatePairing(pairing);
-  }
+  // onResultSubmitted(pairing: Pairing): void {
+  //   this.pairingService.updatePairing(pairing);
+  // }
 
   private createForm(): void {
     this.pairingsForm = this.fb.group({
@@ -100,7 +113,7 @@ export class PairingsPageComponent implements OnDestroy, OnInit {
   private selectedRoundChanged(round: number): void {
     this.selectedRound = round;
 
-    this.pairings$ = this.pairingService.getPairingsForRound(this.selectedRound);
+    // this.pairings$ = this.pairingService.getPairingsForRound(this.selectedRound);
     this.selectedRoundHasPairings$ = this.pairings$.pipe(
       map(pairings => pairings.length > 0),
       distinctUntilChanged()
