a0ad6754bd4cef8cd294f5c52e696fd1b1bfb8d3
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-06-15 21:49:36 -0600

    #68: Cleanup in the match results component.

--- src/app/swiss/components/match-results.component.ts
+++ src/app/swiss/components/match-results.component.ts
@@ -10,12 +10,10 @@ import { matchResultValidator, Pairing, Player } from 'app/shared';
 export class MatchResultsComponent implements OnChanges {
   @Input() playerEntities: Dictionary<Player>;
   @Input() selectedPairing: Pairing;
+  @Input() selectedRoundComplete: boolean;
   @Output() clearMatchResult = new EventEmitter<Pairing>();
   @Output() dropPlayers = new EventEmitter<Player[]>();
   @Output() submitResult = new EventEmitter<Pairing>();
-  // @Output() matchResultCleared = new EventEmitter<Pairing>();
-  // @Output() playerDroppedChanged = new EventEmitter<Player>();
-  // @Output() resultSubmitted = new EventEmitter<Pairing>();
 
   matchResultsForm: FormGroup = this.fb.group({
     player1Wins: 0,
@@ -36,26 +34,29 @@ export class MatchResultsComponent implements OnChanges {
   }
 
   incrementDraws() {
-    const draws = this.matchResultsForm.get('draws').value;
+    const drawsControl = this.matchResultsForm.get('draws');
+    const draws = drawsControl.value;
 
     if (draws < 9) {
-      this.matchResultsForm.patchValue({draws: draws + 1});
+      drawsControl.setValue(draws + 1);
     }
   }
 
   incrementPlayer1Wins() {
-    const player1Wins = this.matchResultsForm.get('player1Wins').value;
+    const player1WinsControl = this.matchResultsForm.get('player1Wins');
+    const player1Wins = player1WinsControl.value;
 
     if (player1Wins < 2) {
-      this.matchResultsForm.patchValue({player1Wins: player1Wins + 1});
+      player1WinsControl.setValue(player1Wins + 1);
     }
   }
 
   incrementPlayer2Wins() {
-    const player2Wins = this.matchResultsForm.get('player2Wins').value;
+    const player2WinsControl = this.matchResultsForm.get('player2Wins');
+    const player2Wins = player2WinsControl.value;
 
     if (player2Wins < 2) {
-      this.matchResultsForm.patchValue({player2Wins: player2Wins + 1});
+      player2WinsControl.setValue(player2Wins + 1);
     }
   }
 
@@ -84,53 +85,7 @@ export class MatchResultsComponent implements OnChanges {
     if (playersToDrop.length > 0) {
       this.dropPlayers.emit(playersToDrop);
     }
-
-    // const form = this.matchResultsForm;
-    // this.selectedPairing.player1Wins = form.get('player1Wins').value;
-    // this.selectedPairing.player2Wins = form.get('player2Wins').value;
-    // this.selectedPairing.draws = form.get('draws').value;
-    // this.selectedPairing.submitted = true;
-    // const player1 = this.selectedPairing.player1;
-    // const player2 = this.selectedPairing.player2;
-    // const player1Dropped = form.get('player1Dropped').value;
-    // const player2Dropped = form.get('player2Dropped').value;
-
-    // if (player1.dropped !== player1Dropped) {
-    //   player1.dropped = player1Dropped;
-    //   this.playerDroppedChanged.emit(player1);
-    // }
-
-    // if (player2.dropped !== player2Dropped) {
-    //   player2.dropped = player2Dropped;
-    //   this.playerDroppedChanged.emit(player2);
-    // }
-
-    // this.resultSubmitted.emit(this.selectedPairing);
   }
-
-  // private createForm() {
-  //   this.matchResultsForm = this.fb.group({
-  //     player1Wins: 0,
-  //     player2Wins: 0,
-  //     draws: 0,
-  //     player1Dropped: false,
-  //     player2Dropped: false
-  //   });
-
-  //   this.matchResultsForm.valueChanges.subscribe(() => this.onValueChanged());
-  // }
-
-  // private onValueChanged() {
-  //   if (!this.matchResultsForm) {
-  //     return;
-  //   }
-
-  //   const form = this.matchResultsForm;
-  //   const player1Wins = form.get('player1Wins').value;
-  //   const player2Wins = form.get('player2Wins').value;
-  //   const draws = form.get('draws').value;
-  //   const gamesPlayed = player1Wins + player2Wins + draws;
-  // }
 
   private resetForm(form: FormGroup, selectedPairing: Pairing | null) {
     if (selectedPairing) {
