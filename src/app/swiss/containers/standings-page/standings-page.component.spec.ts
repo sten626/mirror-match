ea44f1b8430da0206388dd0a3191951102b6aa9d
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-08-04 10:41:06 -0600

    test(swiss): Adding tests for StandingsPageComponent.
    Issue #73

---
+++ src/app/swiss/containers/standings-page/standings-page.component.spec.ts
@@ -0,0 +1,44 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { combineReducers, Store, StoreModule } from '@ngrx/store';
+import * as fromRoot from 'app/reducers';
+import * as fromSwiss from 'app/swiss/reducers';
+import { StandingsPageComponent } from './standings-page.component';
+import { StandingsService } from 'app/shared';
+
+describe('StandingsPageComponent', () => {
+  let component: StandingsPageComponent;
+  let fixture: ComponentFixture<StandingsPageComponent>;
+  let store: Store<fromSwiss.State>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          swiss: combineReducers(fromSwiss.reducers)
+        })
+      ],
+      declarations: [
+        StandingsPageComponent
+      ],
+      providers: [
+        StandingsService
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+    fixture = TestBed.createComponent(StandingsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
