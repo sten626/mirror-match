bed689e2f05dc69c88b8bdd8235b9f42349acb8f
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-11-06 08:38:43 -0600

    test(swiss): Adding regression test for NaN tiebreakers. (#77)

--- src/app/swiss/reducers/util.spec.ts
+++ src/app/swiss/reducers/util.spec.ts
@@ -63,20 +63,47 @@ describe('util', () => {
     }];
   });
 
-  it('calculateStandings should return empty list if there are no players', () => {
-    const result = calculateStandings(pairings, []);
-    expect(result).toEqual([]);
-  });
+  describe('calculateStandings', () => {
+    it('should return empty list if there are no players', () => {
+      const result = calculateStandings(pairings, []);
+      expect(result).toEqual([]);
+    });
 
-  it('calculateStandings should round tiebreakers correctly', () => {
-    const result = calculateStandings(pairings, players);
-    expect(result[0].opponentMatchWinPercentage).toBeCloseTo(5 / 12 * 100);
-    expect(result[0].opponentGameWinPercentage).toBeCloseTo(5 / 12 * 100);
-    expect(result[1].opponentMatchWinPercentage).toBeCloseTo(2 / 3 * 100);
-    expect(result[1].opponentGameWinPercentage).toBeCloseTo(2 / 3 * 100);
-    expect(result[2].opponentMatchWinPercentage).toBeCloseTo(2 / 3 * 100);
-    expect(result[2].opponentGameWinPercentage).toBeCloseTo(2 / 3 * 100);
-    expect(result[3].opponentMatchWinPercentage).toBeCloseTo(5 / 12 * 100);
-    expect(result[3].opponentGameWinPercentage).toBeCloseTo(5 / 12 * 100);
+    it('should round tiebreakers correctly', () => {
+      const result = calculateStandings(pairings, players);
+      expect(result[0].opponentMatchWinPercentage).toBeCloseTo(5 / 12 * 100);
+      expect(result[0].opponentGameWinPercentage).toBeCloseTo(5 / 12 * 100);
+      expect(result[1].opponentMatchWinPercentage).toBeCloseTo(2 / 3 * 100);
+      expect(result[1].opponentGameWinPercentage).toBeCloseTo(2 / 3 * 100);
+      expect(result[2].opponentMatchWinPercentage).toBeCloseTo(2 / 3 * 100);
+      expect(result[2].opponentGameWinPercentage).toBeCloseTo(2 / 3 * 100);
+      expect(result[3].opponentMatchWinPercentage).toBeCloseTo(5 / 12 * 100);
+      expect(result[3].opponentGameWinPercentage).toBeCloseTo(5 / 12 * 100);
+    });
+
+    it('should not return NaNs for players with no opponents', () => {
+      const player5: Player = {
+        id: 5,
+        name: 'Spike',
+        dropped: false
+      };
+      players.push(player5);
+      const pairing: Pairing = {
+        id: player5.id,
+        table: 0,
+        player1Id: 5,
+        player2Id: null,
+        player1Wins: 2,
+        player2Wins: 0,
+        draws: 0,
+        submitted: true
+      };
+      pairings.push(pairing);
+      const results = calculateStandings(pairings, players);
+      const result = results.find((standing) => standing.playerId === player5.id);
+      expect(result.opponentMatchWinPercentage).toBe(0);
+      expect(result.gameWinPercentage).toBe(100);
+      expect(result.opponentGameWinPercentage).toBe(0);
+    });
   });
 });
