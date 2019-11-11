039fa692b9763d868b69a970ae67084f1d881f90
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-11-11 12:37:37 -0600

    test(swiss): Started adding tests for PlayerEffects.
    - Adding ngrx to spellcheck.
    - Removing rxjs-compat dependency as it shouldn't be needed anymore.
    - Adding jasmine-marbles dependency.
    
    Issue #80.

---
+++ src/app/swiss/effects/player.effects.spec.ts
@@ -0,0 +1,58 @@
+import { TestBed } from '@angular/core/testing';
+import { Actions } from '@ngrx/effects';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Player, PlayerStorageService } from 'app/shared';
+import { cold, hot } from 'jasmine-marbles';
+import { Observable } from 'rxjs';
+import { PlayersApiActions, PlayersPageActions } from '../actions';
+import { PlayerEffects } from './player.effects';
+
+describe('PlayerEffects', () => {
+  let actions$: Observable<any>;
+  let effects: PlayerEffects;
+  const storageSpy = jasmine.createSpyObj(
+    'PlayerStorageService', [
+      'addPlayer',
+      'getPlayers'
+    ]
+  );
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: PlayerStorageService, useValue: storageSpy },
+        PlayerEffects,
+        provideMockActions(() => actions$)
+      ]
+    });
+
+    effects = TestBed.get(PlayerEffects);
+    actions$ = TestBed.get(Actions);
+  });
+
+  it('should be created', () => {
+    expect(effects).toBeTruthy();
+  });
+
+  describe('addPlayer', () => {
+    it('should call storage service', () => {
+      const action = PlayersPageActions.addPlayer({
+        playerName: 'Steven'
+      });
+      const player: Player = {
+        id: 1,
+        name: 'Steven',
+        dropped: false
+      };
+      const completion = PlayersApiActions.addPlayerSuccess({
+        'player': player
+      });
+
+      actions$ = hot('-a', { a: action });
+      // const response = cold('-a|', { a: player });
+      const expected = cold('--c', { c: completion });
+
+      expect(effects.addPlayer$).toBeObservable(expected);
+    });
+  });
+});
