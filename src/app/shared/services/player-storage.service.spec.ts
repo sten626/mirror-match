cee88cf5b42226df56d45f0a459025d18e37d7db
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-11-15 08:13:05 -0600

    test(shared): Adding test to check that PlayerStorageService doesn't change an existing ID.
    - Currently failing.
    - Regression test.
    
    Issue #80

--- src/app/shared/services/player-storage.service.spec.ts
+++ src/app/shared/services/player-storage.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Player } from 'app/shared/models';
 import { PlayerStorageService } from './player-storage.service';
-import { of } from 'rxjs';
 
 describe('PlayerStorageService', () => {
   let service: PlayerStorageService;
@@ -27,15 +26,17 @@ describe('PlayerStorageService', () => {
   });
 
   describe('addPlayer', () => {
-    it('should not change the passed in ID', () => {
+    it('should not change the passed in ID', (done: DoneFn) => {
       const player: Player = {
         id: 5,
         name: 'Steven',
         dropped: false
       };
-      const expected = of(player);
-      const result = service.addPlayer(player);
-      expect(result).toBeObservable(expected);
+
+      service.addPlayer(player).subscribe(value => {
+        expect(value).toEqual(player);
+        done();
+      });
     });
   });
 });
