168d740feb90cf1b4d6aeee877117bb3e384f17e
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-11-30 21:14:39 -0600

    feat: Adding feather icons to project.
    - Apparently Bootstrap 4 removed icons?
    
    Issue #69

--- src/app/shared/layout/footer.component.ts
+++ src/app/shared/layout/footer.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as feather from 'feather-icons';
 
 @Component({
   selector: 'mm-layout-footer',
   styleUrls: ['./footer.component.css'],
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   today: number = Date.now();
   version = require('../../../../package.json').version;
+
+  ngOnInit() {
+    feather.replace();
+  }
 }
