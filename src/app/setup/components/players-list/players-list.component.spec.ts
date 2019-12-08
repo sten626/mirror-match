b047367fbe97350b3083f1522730e8858b454803
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-12-07 23:06:12 -0600

    test(setup): Fixing PlayersListComponentSpec.
    Issue #69

--- src/app/setup/components/players-list/players-list.component.spec.ts
+++ src/app/setup/components/players-list/players-list.component.spec.ts
@@ -1,14 +1,23 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PlayersListComponent } from './players-list.component';
 
 describe('PlayersListComponent', () => {
   let component: PlayersListComponent;
   let fixture: ComponentFixture<PlayersListComponent>;
 
+  @Component({selector: 'mm-players-list-item', template: ''})
+  class PlayersListItemStubComponent {}
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayersListComponent ]
+      declarations: [
+        PlayersListComponent,
+        PlayersListItemStubComponent
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     })
     .compileComponents();
   }));
