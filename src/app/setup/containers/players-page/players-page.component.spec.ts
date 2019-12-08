2f4e3fd71803cf574d1ce95a11247d024deab282
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-12-07 23:01:45 -0600

    test(setup): Fixing PlayersPageComponentSpec.
    Issue #69

--- src/app/setup/containers/players-page/players-page.component.spec.ts
+++ src/app/setup/containers/players-page/players-page.component.spec.ts
@@ -1,14 +1,28 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import * as fromRoot from '@app/reducers';
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { PlayersPageComponent } from './players-page.component';
 
 describe('PlayersPageComponent', () => {
   let component: PlayersPageComponent;
   let fixture: ComponentFixture<PlayersPageComponent>;
+  let store: MockStore<fromRoot.State>;
+
+  @Component({selector: 'mm-player-form', template: ''})
+  class PlayerFormStubComponent {}
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayersPageComponent ]
+      declarations: [
+        PlayerFormStubComponent,
+        PlayersPageComponent
+      ],
+      providers: [
+        provideMockStore()
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -16,7 +30,13 @@ describe('PlayersPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayersPageComponent);
     component = fixture.componentInstance;
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch');
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create', () => {
