f348798b098ef44d188976569b9f54ac93df5f4e
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-12-12 08:20:55 -0600

    feat: Changing AppComponent to use FlexLayoutModule
    Issue #69

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -31,6 +32,7 @@ import { environment } from '../environments/environment';
     CommonModule,
     CoreModule,
     EffectsModule.forRoot([PlayerEffects]),
+    FlexLayoutModule,
     FormsModule,
     HomeModule,
     MatButtonModule,
