7f090ed301dc8a1ba2b4788d2a98ab9794f68107
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-12-16 09:17:13 -0600

    feat(core): Working on styling the footer.
    Issue #69

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'mm-layout-footer',
   styleUrls: ['./footer.component.css'],
   templateUrl: './footer.component.html'
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
   today: number = Date.now();
-  version = require('../../../../../package.json').version;
-
-  ngOnInit() {
-    // feather.replace({
-    //   height: '1rem',
-    //   width: '1rem'
-    // });
-  }
+  version: string = require('../../../../../package.json').version;
 }
