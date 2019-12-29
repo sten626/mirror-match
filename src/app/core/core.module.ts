a889b07d59a53dbcf08a3eda6e9d04bc936dc7a0
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-12-29 10:17:43 -0600

    feat(core): Removing header from sidenav.
    - Since it's under the toolbar now this isn't needed.
    
    Issue #85

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -22,7 +21,6 @@ const COMPONENTS = [
     CommonModule,
     MatButtonModule,
     MatCardModule,
-    MatDividerModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
