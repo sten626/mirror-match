11f7bfd3918206b93770c65a085f7a23c1e4dd40
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-12-29 23:46:39 -0600

    feat(setup): Starting on content of PlayerEditDialogComponent.
    Issue #85

--- src/app/setup/setup.module.ts
+++ src/app/setup/setup.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { PlayerEditDialogComponent, PlayerFormComponent, PlayersListComponent, PlayersListItemComponent } from '@app/setup/components';
 import { PlayersPageComponent, SetupPageComponent } from '@app/setup/containers';
@@ -26,7 +28,9 @@ const CONTAINERS = [
     CommonModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     ReactiveFormsModule,
     SetupRoutingModule
