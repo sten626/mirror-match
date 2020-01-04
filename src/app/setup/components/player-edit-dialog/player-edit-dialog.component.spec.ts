363b2fce57551a342b6c9976e0597567bffc7d14
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-01-04 11:48:32 -0600

    test(setup): Updating test for PlayerEditDialog.
    - Adding Material modules.
    - Adding NoopAnimationsModule.
    - Adding ReactiveFormsModule.
    - Adding provider for MAT_DIALOG_DATA.
    
    Issue #85

--- src/app/setup/components/player-edit-dialog/player-edit-dialog.component.spec.ts
+++ src/app/setup/components/player-edit-dialog/player-edit-dialog.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Player } from '@app/shared/models';
 import { PlayerEditDialogComponent } from './player-edit-dialog.component';
 
 describe('PlayerEditDialogComponent', () => {
   let component: PlayerEditDialogComponent;
   let fixture: ComponentFixture<PlayerEditDialogComponent>;
+  const player: Player = {
+    id: 1,
+    name: 'Sten',
+    dropped: false
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayerEditDialogComponent ]
+      imports: [
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: {player}}
+      ],
+      declarations: [
+        PlayerEditDialogComponent
+      ]
     })
     .compileComponents();
   }));
