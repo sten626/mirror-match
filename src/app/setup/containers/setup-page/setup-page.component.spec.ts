57a15fdfc5256a1f0f8d7bd84636d8f60e9481cb
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-01-04 11:28:12 -0600

    test(setup): Fixing tests for SetupPage.
    - Stubbing PlayerForm.
    - Importing MatDialogModule.
    - Creating MockStore.
    
    Issue #85

--- src/app/setup/containers/setup-page/setup-page.component.spec.ts
+++ src/app/setup/containers/setup-page/setup-page.component.spec.ts
@@ -1,20 +1,37 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterTestingModule } from '@angular/router/testing';
+import * as fromRoot from '@app/reducers';
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { SetupPageComponent } from './setup-page.component';
 
 describe('SetupPageComponent', () => {
   let component: SetupPageComponent;
   let fixture: ComponentFixture<SetupPageComponent>;
+  let store: MockStore<fromRoot.State>;
+
+  @Component({selector: 'mm-player-form', template: ''})
+  class PlayerFormStubComponent {}
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        MatDialogModule,
         MatTabsModule,
         RouterTestingModule
       ],
       declarations: [
+        PlayerFormStubComponent,
         SetupPageComponent
+      ],
+      providers: [
+        provideMockStore()
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
       ]
     })
     .compileComponents();
@@ -23,7 +40,13 @@ describe('SetupPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SetupPageComponent);
     component = fixture.componentInstance;
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch');
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create', () => {
