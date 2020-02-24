ed1df1d61cf51c0ffa1400240a0fd2e76a38cee9
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-02-23 22:06:27 -0600

    test(core): Adding test in TournamentReducer.
    - startTournamentSuccess test.
    
    Issue #87

--- src/app/core/reducers/tournament.reducer.spec.ts
+++ src/app/core/reducers/tournament.reducer.spec.ts
@@ -1,4 +1,6 @@
 import { reducer, initialState } from './tournament.reducer';
+import { TournamentApiActions } from '@app/core/actions';
+
 
 describe('Tournament Reducer', () => {
   describe('an unknown action', () => {
@@ -8,6 +10,24 @@ describe('Tournament Reducer', () => {
       const result = reducer(initialState, action);
 
       expect(result).toBe(initialState);
+    });
+  });
+
+  describe('startTournamentSuccess', () => {
+    it('should set all 3 values to the state', () => {
+      const action = TournamentApiActions.startTournamentSuccess({
+        bestOf: 3,
+        isDraft: false,
+        totalRounds: 3
+      });
+      const state = reducer(initialState, action);
+
+      expect(state).toEqual({
+        ...initialState,
+        bestOf: 3,
+        isDraft: false,
+        totalRounds: 3
+      });
     });
   });
 });
