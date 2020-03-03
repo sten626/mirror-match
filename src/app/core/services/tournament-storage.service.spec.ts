cc70ad5c9ee15f286a10a05f276b4ef03cea1db6
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-03-03 09:17:52 -0600

    test(core): Adding test for TournamentStorageService.
    - setBestOf test.
    
    Issue #87

--- src/app/core/services/tournament-storage.service.spec.ts
+++ src/app/core/services/tournament-storage.service.spec.ts
@@ -1,12 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
+import { LOCAL_STORAGE_TOKEN } from '@app/core/services/storage.service';
 import { TournamentStorageService } from './tournament-storage.service';
 
 describe('TournamentStorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: TournamentStorageService;
+  let storageSpy: any;
+
+  beforeEach(() => {
+    storageSpy = jasmine.createSpyObj('Storage', [
+      'getItem',
+      'removeItem',
+      'setItem'
+    ]);
+    TestBed.configureTestingModule({
+      providers: [
+        TournamentStorageService,
+        { provide: LOCAL_STORAGE_TOKEN, useValue: storageSpy }
+      ]
+    });
+
+    service = TestBed.get(TournamentStorageService);
+  });
 
   it('should be created', () => {
-    const service: TournamentStorageService = TestBed.get(TournamentStorageService);
     expect(service).toBeTruthy();
+  });
+
+  describe('setBestOf', () => {
+    it('should set and return the value', (done: DoneFn) => {
+      const value = 5;
+      service.setBestOf(value).subscribe(result => {
+        expect(result).toBe(value);
+        expect(storageSpy.setItem).toHaveBeenCalledWith('mm-best-of', value.toString());
+        done();
+      });
+    });
   });
 });
