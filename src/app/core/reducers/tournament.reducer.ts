6d1fe7db91a14fd03a3d4479775d028115887999
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-04-21 09:17:45 -0600

    feat(core): Creating pod building function.
    - Adding pods to tournament state.
    
    Issue #89

--- src/app/core/reducers/tournament.reducer.ts
+++ src/app/core/reducers/tournament.reducer.ts
@@ -2,17 +2,20 @@ import { TournamentApiActions } from '@app/core/actions';
 import { Action, createReducer, on } from '@ngrx/store';
 
 export const tournamentFeatureKey = 'tournament';
+export type Pod = number[];
 
 export interface State {
   bestOf: number;
   isDraft: boolean;
   totalRounds: number;
+  pods: Pod[];
 }
 
 export const initialState: State = {
   bestOf: 0,
   isDraft: true,
-  totalRounds: 0
+  totalRounds: 0,
+  pods: []
 };
 
 const tournamentReducer = createReducer(
---
+++ src/app/core/util.spec.ts
@@ -0,0 +1,47 @@
+import { buildPods } from '@app/core/util';
+
+function getMockPlayerIds(numPlayers: number): number[] {
+  const playerIds: number[] = [];
+
+  for (let i = 0; i < numPlayers; i++) {
+    playerIds.push(i);
+  }
+
+  return playerIds;
+}
+
+describe('util', () => {
+  describe('buildPods', () => {
+    const data = new Map<number, number[]>([
+      [6, [6]],
+      [7, [7]],
+      [8, [8]],
+      [9, [9]],
+      [10, [10]],
+      [11, [11]],
+      [12, [6, 6]],
+      [13, [7, 6]],
+      [14, [7, 7]],
+      [15, [8, 7]],
+      [16, [8, 8]],
+      [17, [9, 8]],
+      [18, [6, 6, 6]],
+      [19, [7, 6, 6]],
+      [20, [7, 7, 6]],
+      [25, [7, 6, 6, 6]]
+    ]);
+
+    data.forEach((value, key) => {
+      it(`should return pods ${value} with ${key} players`, () => {
+        const playerIds = getMockPlayerIds(key);
+        const pods = buildPods(playerIds);
+        const expectedNumPods = value.length;
+        expect(pods.length).toBe(expectedNumPods);
+
+        value.forEach((podSize, i) => {
+          expect(pods[i].length).toBe(podSize);
+        });
+      });
+    });
+  });
+});
