c828e2172f023187952abc9bb25356891e76fb89
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-07-07 21:47:38 -0600

    test(core): Updating tests.
    - Fixing ToolbarComponent spec.
    - Deleting LayoutComponent.
    
    Issue #96

--- src/app/core/components/toolbar/toolbar.component.spec.ts
+++ src/app/core/components/toolbar/toolbar.component.spec.ts
@@ -1,26 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import * as fromRoot from '@app/reducers';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
+  let mockStore: MockStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        MatIconModule,
-        MatToolbarModule
-      ],
-      declarations: [
-        ToolbarComponent
-      ]
-    })
-    .compileComponents();
+      imports: [MatToolbarModule],
+      declarations: [ToolbarComponent],
+      providers: [provideMockStore()]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    mockStore = TestBed.inject(MockStore);
+    mockStore.overrideSelector(fromRoot.hasAnythingStarted, false);
+    mockStore.overrideSelector(fromRoot.hasDraftStarted, false);
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
