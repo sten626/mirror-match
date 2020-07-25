c25c6c60423089ecb3f1c6e826a5a5ee40ccb48c
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-07-25 07:38:04 -0600

    feat(core): Working on bottom sheet overlay.
    Issue #96

--- src/app/core/services/bottom-sheet.service.ts
+++ src/app/core/services/bottom-sheet.service.ts
@@ -1,35 +1,41 @@
-import { Injectable, Injector } from '@angular/core';
-import {
-  createCustomElement,
-  NgElement,
-  WithProperties
-} from '@angular/elements';
+import { Overlay, OverlayRef, OverlayConfig } from '@angular/cdk/overlay';
+import { Injectable } from '@angular/core';
 import { BottomSheetContainerComponent } from '@app/core/components';
+import { ComponentPortal } from '@angular/cdk/portal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BottomSheetService {
-  constructor(injector: Injector) {
-    const bottomSheetContainerEl = createCustomElement(
-      BottomSheetContainerComponent,
-      { injector }
-    );
-    customElements.define('mm-bottom-sheet-container', bottomSheetContainerEl);
-  }
+  constructor(private overlay: Overlay) {}
 
   open() {
-    this.createContainer();
+    const overlayRef = this.createOverlay();
+    // const bottomSheetContainer = this.attachBottomSheetContainer(overlayRef);
+    this.attachBottomSheetContainer(overlayRef);
   }
 
-  private createContainer() {
-    const bottomSheetContainerEl: NgElement &
-      WithProperties<BottomSheetContainerComponent> = document.createElement(
-      'mm-bottom-sheet-container'
-    ) as any;
-    bottomSheetContainerEl.addEventListener('close', () =>
-      document.body.removeChild(bottomSheetContainerEl)
-    );
-    document.body.appendChild(bottomSheetContainerEl);
+  private attachBottomSheetContainer(overlay: OverlayRef): BottomSheetContainerComponent {
+    const containerPortal = new ComponentPortal(BottomSheetContainerComponent);
+    const containerRef = overlay.attach(containerPortal);
+
+    return containerRef.instance;
+  }
+
+  private createOverlay(): OverlayRef {
+    const overlayConfig = this.getOverlayConfig();
+    return this.overlay.create(overlayConfig);
+  }
+
+  private getOverlayConfig(): OverlayConfig {
+    const config = new OverlayConfig({
+      hasBackdrop: true,
+      minHeight: '56px',
+      positionStrategy: this.overlay.position().global().bottom('0').left('0'),
+      scrollStrategy: this.overlay.scrollStrategies.block(),
+      width: '100%'
+    });
+
+    return config;
   }
 }
