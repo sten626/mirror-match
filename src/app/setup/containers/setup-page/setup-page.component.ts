614b6d9700f06223585335fe565fdcb4563414fe
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-08-11 21:50:47 -0600

    feat(setup): Fixing issues with centreing no players message.
    - Changing toolbars to work with flexbox instead of fixed positioning.
    - This allows me to cleanup a little breakpoint code.
    
    Issue #96

--- src/app/setup/containers/setup-page/setup-page.component.ts
+++ src/app/setup/containers/setup-page/setup-page.component.ts
@@ -1,4 +1,3 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as fromRoot from '@mm/reducers';
@@ -15,20 +14,12 @@ import { map } from 'rxjs/operators';
 })
 export class SetupPageComponent {
   isNextButtonEnabled$: Observable<boolean>;
-  isXSmallDisplay$: Observable<boolean>;
 
-  constructor(
-    private breakpointObserver: BreakpointObserver,
-    private store: Store<fromRoot.State>
-  ) {
+  constructor(private store: Store<fromRoot.State>) {
     this.isNextButtonEnabled$ = this.store.pipe(
       select(fromRoot.selectAllPlayers),
       map((players) => players.length >= 4)
     );
-
-    this.isXSmallDisplay$ = this.breakpointObserver
-      .observe(Breakpoints.XSmall)
-      .pipe(map((result) => result.matches));
   }
 
   prepareRoute(outlet: RouterOutlet) {
