d8e0c7d71c5df443d4064696c874326514d9cd55
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-09-01 22:12:48 -0600

    test(setup): Updating components used in SetupPage spec.
    Issue #96

--- src/app/setup/containers/setup-page/setup-page.component.spec.ts
+++ src/app/setup/containers/setup-page/setup-page.component.spec.ts
@@ -1,9 +1,11 @@
+import { Component, Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import * as fromRoot from '@mm/reducers';
+import { Player } from '@mm/shared/models';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { SetupPageComponent } from './setup-page.component';
 
@@ -12,10 +14,30 @@ describe('SetupPageComponent', () => {
   let fixture: ComponentFixture<SetupPageComponent>;
   let store: MockStore;
 
+  // tslint:disable-next-line: component-selector
+  @Component({ selector: 'mat-sidenav-container', template: '' })
+  class MatSidenavContainerStubComponent {}
+
+  // tslint:disable-next-line: component-selector
+  @Component({ selector: 'mat-sidenav', template: '' })
+  class MatSidenavStubComponent {
+    @Input() fixedInViewport: boolean;
+  }
+
+  @Component({ selector: 'mm-players-list', template: '' })
+  class PlayersListStubComponent {
+    @Input() players: Player[];
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       providers: [provideMockStore()],
-      declarations: [SetupPageComponent],
+      declarations: [
+        MatSidenavContainerStubComponent,
+        MatSidenavStubComponent,
+        PlayersListStubComponent,
+        SetupPageComponent
+      ],
       imports: [
         MatIconModule,
         MatToolbarModule,
