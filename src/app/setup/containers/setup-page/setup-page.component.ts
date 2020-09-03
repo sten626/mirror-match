c1b9119e645f420470f7122e2855ef0bafbc1a34
Steven Indzeoski <steven.indzeoski@gmail.com>
2020-09-02 22:03:16 -0600

    feat(setup): Got the setup page mostly how I want it.
    - Adding start button to side sheet.
    - Getting non-mobile display set up.
    
    Issue #96

--- src/app/setup/containers/setup-page/setup-page.component.ts
+++ src/app/setup/containers/setup-page/setup-page.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as fromRoot from '@mm/reducers';
 import { Player } from '@mm/shared/models';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'mm-setup-page',
   templateUrl: './setup-page.component.html',
   styleUrls: ['./setup-page.component.scss']
 })
-export class SetupPageComponent {
+export class SetupPageComponent implements OnInit, OnDestroy {
+  breakpointSub: Subscription;
   players$: Observable<Player[]>;
   recommendedTotalRounds$: Observable<number>;
+  sheetFixed = true;
+  sheetMode = 'over';
+  sheetOpened = false;
 
-  constructor(store: Store<fromRoot.State>) {
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    store: Store<fromRoot.State>
+  ) {
     this.players$ = store.pipe(select(fromRoot.selectAllPlayers));
     this.recommendedTotalRounds$ = store.pipe(
       select(fromRoot.selectRecommendedTotalRounds)
     );
+  }
+
+  ngOnInit() {
+    this.breakpointSub = this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.sheetFixed = true;
+          this.sheetMode = 'over';
+          this.sheetOpened = false;
+        } else {
+          this.sheetFixed = false;
+          this.sheetMode = 'side';
+          this.sheetOpened = true;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.breakpointSub.unsubscribe();
   }
 }
