7643beb0ca2b0d8444dc53a2d8c939ae441d0370
Steven Indzeoski <steven.indzeoski@gmail.com>
2021-02-12 22:44:32 -0600

    feat(setup): Working on add player list animation.
    - Also disabling autocomplete on player name field.
    
    Issue #96

--- src/app/setup/organisms/players-list/players-list.component.ts
+++ src/app/setup/organisms/players-list/players-list.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { playerListAnimations } from '@mm/setup/organisms/players-list/player-list-animations';
 import { Player } from '@mm/shared/models';
 
 @Component({
   selector: 'mm-players-list',
+  animations: playerListAnimations,
   templateUrl: './players-list.component.html',
   styleUrls: ['./players-list.component.scss']
 })
-export class PlayersListComponent {
+export class PlayersListComponent implements OnInit {
   @Input() players: Player[];
 
+  initialPlayers: Set<Player>;
+
   constructor() {}
+
+  ngOnInit() {
+    this.initialPlayers = new Set(this.players);
+  }
 }
