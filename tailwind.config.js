9aefadf6e80cfed8201298d998489e8703a96942
Steven Indzeoski <steven.indzeoski@gmail.com>
2022-06-02 22:14:54 -0600

    feat(shared): Working on TopAppBar.
    - Adding tailwind prettier plugin.
    - Fixing issue in the tokens file that all the typography tokens had
      rem instead of px.
    - Styling TopAppBar.
    
    Issue #96

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     colors: {
       primary: "var(--md-sys-color-primary)",
       surface: "var(--md-sys-color-surface)",
+      "on-surface": "var(--md-sys-color-on-surface)"
+    },
+    fontFamily: {
+      "title-large": "var(--md-sys-typescale-title-large-family)"
+    },
+    fontSize: {
+      "title-large": "var(--md-sys-typescale-title-large-size)"
+    },
+    fontWeight: {
+      "title-large": "var(--md-sys-typescale-title-large-weight)"
+    },
+    lineHeight: {
+      "title-large": 28
     },
     extend: {},
   },
