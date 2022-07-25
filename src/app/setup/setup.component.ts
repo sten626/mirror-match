2b3276f9fcf095e934f10b7116d1042ae2ca0f50
Steven Indzeoski <steven.indzeoski@gmail.com>
2022-07-25 13:27:28 -0600

    feat(setup): Tweaking cards on setup page
    - Adding strictTemplates flag in compiler
    
    Issue #96

--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Card {
+  title: string;
+  body: string;
+  disabled?: boolean;
+}
+
 @Component({
   selector: 'mm-setup',
   templateUrl: './setup.component.html',
   styleUrls: ['./setup.component.scss'],
 })
 export class SetupComponent {
+  cards: Card[] = [
+    {
+      title: 'Swiss Pairing',
+      body: 'A non-eliminating tournament structure with a fixed number of rounds. Each round players play against opponents with a similar score.',
+    },
+    {
+      title: 'Draft Pods',
+      body: 'Coming soon.',
+      disabled: true,
+    },
+  ];
+
   constructor() {}
 }
