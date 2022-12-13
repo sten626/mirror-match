e3596c924b7b5f5c532e73c15c03497428434c17
Steven Indzeoski <steven.indzeoski@gmail.com>
2022-12-12 21:57:57 -0600

    Fixing errors with root reducers.

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,20 +1,22 @@
-import { ActionReducerMap, Action, createFeatureSelector, createSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromMessages from 'app/core/reducers/messages.reducer';
-import { InjectionToken } from '@angular/core';
 
 export interface State {
   [fromMessages.messagesFeatureKey]: fromMessages.State;
 }
 
-export const rootReducers = new InjectionToken<
-  ActionReducerMap<State, Action>
->('Root reducers token', {
-  factory: () => ({
-    [fromMessages.messagesFeatureKey]: fromMessages.reducer
-  })
-});
+// export const rootReducers = new InjectionToken<
+//   ActionReducerMap<State, Action>
+// >('Root reducers token', {
+//   factory: () => ({
+//     [fromMessages.messagesFeatureKey]: fromMessages.reducer
+//   })
+// });
+export const rootReducers = {
+  [fromMessages.messagesFeatureKey]: fromMessages.reducer,
+};
 
-export const getMessagesState = createFeatureSelector< fromMessages.State>(
+export const getMessagesState = createFeatureSelector<fromMessages.State>(
   fromMessages.messagesFeatureKey
 );
 
