98fde56bf843373d4f3f5ad16a4dd7151abcec92
Steven Indzeoski <steven.indzeoski@gmail.com>
2023-01-02 11:33:10 -0600

    Removing deprecated function calls.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,21 @@
-/* tslint:disable:no-unused-variable */
-
-import { async, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    })
-    .compileComponents();
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  // it('should create the app', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
 
-  // it(`should have as title 'app works!'`, async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('app works!');
-  // }));
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  // }));
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
 });
--- src/app/swiss/swiss.module.ts
+++ src/app/swiss/swiss.module.ts
@@ -13,20 +13,36 @@ import {
   RoundInfoComponent,
   StandingsTableComponent,
   StartFormComponent,
-  SwissMenuComponent
+  SwissMenuComponent,
 } from 'app/swiss/components';
-import { EventInfoComponent, PairingsPageComponent, PlayersPageComponent, StandingsPageComponent, SwissComponent } from 'app/swiss/containers';
-import { PairingEffects, PlayerEffects, RoundEffects, SwissEffects } from 'app/swiss/effects';
+import {
+  EventInfoComponent,
+  PairingsPageComponent,
+  PlayersPageComponent,
+  StandingsPageComponent,
+  SwissComponent,
+} from 'app/swiss/containers';
+import {
+  PairingEffects,
+  PlayerEffects,
+  RoundEffects,
+  SwissEffects,
+} from 'app/swiss/effects';
 import { StandingsGuard, TournamentStartedGuard } from 'app/swiss/guards';
 import * as fromSwiss from 'app/swiss/reducers';
 import { SwissRoutingModule } from 'app/swiss/swiss-routing.module';
 
 @NgModule({
   imports: [
-    EffectsModule.forFeature([PairingEffects, PlayerEffects, RoundEffects, SwissEffects]),
+    EffectsModule.forFeature([
+      PairingEffects,
+      PlayerEffects,
+      RoundEffects,
+      SwissEffects,
+    ]),
     SharedModule,
     StoreModule.forFeature(fromSwiss.swissFeatureKey, fromSwiss.reducers),
-    SwissRoutingModule
+    SwissRoutingModule,
   ],
   declarations: [
     EventInfoComponent,
@@ -44,11 +60,8 @@ import { SwissRoutingModule } from 'app/swiss/swiss-routing.module';
     SwissComponent,
     SwissMenuComponent,
     StandingsPageComponent,
-    StandingsTableComponent
+    StandingsTableComponent,
   ],
-  providers: [
-    StandingsGuard,
-    TournamentStartedGuard
-  ]
+  providers: [StandingsGuard, TournamentStartedGuard],
 })
-export class SwissModule { }
+export class SwissModule {}
