8aef8954fe9b255dab1de9c4994c036961bca366
Steven Indzeoski <steven.indzeoski@gmail.com>
2023-01-23 12:46:56 -0600

    Fixing failing AppComponent tests.
    - Stubbing child components.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,15 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+@Component({ selector: 'mm-layout-footer', template: '' })
+class LayoutFooterComponentStub {}
+
+@Component({ selector: 'mm-layout-header', template: '' })
+class LayoutHeaderComponentStub {}
+
+@Component({ selector: 'router-outlet', template: '' })
+class RouterOutletComponentStub {}
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -7,7 +17,12 @@ describe('AppComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [
+        AppComponent,
+        LayoutFooterComponentStub,
+        LayoutHeaderComponentStub,
+        RouterOutletComponentStub,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
