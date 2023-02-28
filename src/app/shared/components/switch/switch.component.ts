442934003231ccf6153d4c8ab2f1196d50d54018
Steven Indzeoski <steven.indzeoski@gmail.com>
2023-02-28 08:10:14 -0600

    WIP SwitchComponent.
    - Setting package manager to pnpm
    - Adding extra eslint packages
    - Removing old unused packages
    
    Issue #115

--- src/app/shared/components/switch/switch.component.ts
+++ src/app/shared/components/switch/switch.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mm-switch',
   templateUrl: './switch.component.html',
-  styleUrls: ['./switch.component.scss']
+  styleUrls: ['./switch.component.scss'],
 })
 export class SwitchComponent {
+  @Input()
+  disabled = false;
 
+  @Input()
+  selected = false;
+
+  handleClick() {
+    if (this.disabled) {
+      return;
+    }
+
+    this.selected = !this.selected;
+  }
+
+  styleClasses(): Record<string, boolean> {
+    return {
+      selected: this.selected,
+    };
+  }
 }
